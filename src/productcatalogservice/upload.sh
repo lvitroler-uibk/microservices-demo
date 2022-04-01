6f9708895ed8830d9661fbfb5650c5bceb580370
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-04-01 12:50:14 +0200

    feat: try with tag numbers

--- src/productcatalogservice/upload.sh
+++ src/productcatalogservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-products:v1.0 .
-docker push lvitroleruibk/boutique-products:v1.0
+docker build -t lvitroleruibk/boutique-products:v1.1 .
+docker push lvitroleruibk/boutique-products:v1.1
