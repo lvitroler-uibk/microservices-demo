6aecb6223b13f614f96dadc5400e18bc1b17c68e
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-05-09 18:22:41 +0200

    feat: update tag

--- src/frontend/upload.sh
+++ src/frontend/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-frontend:v1.1 .
-docker push lvitroleruibk/boutique-frontend:v1.1
+docker build -t lvitroleruibk/boutique-frontend:v1.2 .
+docker push lvitroleruibk/boutique-frontend:v1.2
