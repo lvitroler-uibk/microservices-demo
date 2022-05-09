6578d8cd5118998c5c9896f732d01f7cc068c97d
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-05-09 15:07:34 +0200

    fix: stupid false names

--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifying:v1.2 .
-docker push lvitroleruibk/boutique-classifying:v1.2
+docker build -t lvitroleruibk/boutique-classifier:v1.0 .
+docker push lvitroleruibk/boutique-classifier:v1.0
