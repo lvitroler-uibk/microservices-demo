f953808851d91acca2be7fd0e5e385bf04753ec7
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-05-11 14:16:57 +0200

    fix: assign is wrong

--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v1.5 .
-docker push lvitroleruibk/boutique-classifier:v1.5
+docker build -t lvitroleruibk/boutique-classifier:v1.6 .
+docker push lvitroleruibk/boutique-classifier:v1.6
