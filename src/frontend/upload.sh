cb416df0c2d404d482f678df3df8236e15d4d4e6
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-05-11 14:23:14 +0200

    fix: use correct variable in html template #2

--- src/frontend/upload.sh
+++ src/frontend/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-frontend:v1.7 .
-docker push lvitroleruibk/boutique-frontend:v1.7
+docker build -t lvitroleruibk/boutique-frontend:v1.8 .
+docker push lvitroleruibk/boutique-frontend:v1.8
