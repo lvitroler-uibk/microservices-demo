9b629f17fbbf1c5cd665e9356d5e9b31c32860dd
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-05-15 19:35:29 +0200

    feat: use http

--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v1.23 .
-docker push lvitroleruibk/boutique-classifier:v1.23
+docker build -t lvitroleruibk/boutique-classifier:v1.24 .
+docker push lvitroleruibk/boutique-classifier:v1.24
