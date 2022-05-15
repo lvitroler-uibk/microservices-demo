c83915d99637c905fe51819c5a56228ab57ae808
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-05-15 16:34:09 +0200

    fix datetime import

--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v1.11 .
-docker push lvitroleruibk/boutique-classifier:v1.11
+docker build -t lvitroleruibk/boutique-classifier:v1.12 .
+docker push lvitroleruibk/boutique-classifier:v1.12
