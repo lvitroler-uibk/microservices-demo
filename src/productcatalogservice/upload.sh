ad226b093c75c5d6b58137046e7fec546ea7e860
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-05-15 20:47:46 +0200

    feat: new pics

--- src/productcatalogservice/upload.sh
+++ src/productcatalogservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-products:v1.2 .
-docker push lvitroleruibk/boutique-products:v1.2
+docker build -t lvitroleruibk/boutique-products:v1.3 .
+docker push lvitroleruibk/boutique-products:v1.3
