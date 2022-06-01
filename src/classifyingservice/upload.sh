7a04d244bd5f455c6b5e0d989bb0d929f421d50d
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-06-01 14:18:11 +0200

    feat: move model to root

--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v2.1 .
-docker push lvitroleruibk/boutique-classifier:v2.1
+docker build -t lvitroleruibk/boutique-classifier:v2.2 .
+docker push lvitroleruibk/boutique-classifier:v2.2
