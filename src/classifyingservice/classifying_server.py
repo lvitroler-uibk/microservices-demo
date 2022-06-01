1d33c6cc94402519ace8fb6b865dfe299a0fd79c
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-06-01 13:52:15 +0200

    refact: move classifier to its own file

--- src/classifyingservice/classifying_server.py
+++ src/classifyingservice/classifying_server.py
@@ -19,6 +19,7 @@ import os
 import time
 import traceback
 from concurrent import futures
+from classifyingservice.classifier.predict import Classifier
 
 import googleclouddebugger
 import googlecloudprofiler
@@ -33,15 +34,6 @@ import demo_pb2
 import demo_pb2_grpc
 from grpc_health.v1 import health_pb2
 from grpc_health.v1 import health_pb2_grpc
-
-from PIL import Image
-from torchvision import models, transforms
-from io import BytesIO
-
-import torch
-import torch.nn as nn
-import requests
-import socket
 
 from logger import getJSONLogger
 logger = getJSONLogger('classifyingservice-server')
@@ -79,26 +71,16 @@ class ClassifyingService(demo_pb2_grpc.ClassifyingServiceServicer):
         # fetch list of products from product catalog stub
         request = demo_pb2.GetProductRequest()
         request.id = prod_id
-
         product = product_catalog_stub.GetProduct(request)
 
         logger.info("[Recv ListClassifyings] product_id={}".format(prod_id))
         logger.info("[Recv ListClassifyings] host={}".format(host))
         logger.info("[Recv ListClassifyings] picture={}".format(product.picture))
 
-        hostname = socket.gethostname()
-        local_ip = socket.gethostbyname(hostname)
-        logger.info("[Recv ListClassifyings] local={}".format(local_ip))
-
         # https://www.kaggle.com/code/pavelgot/items-classification-pytorch/notebook
         #response = requests.get("https://static.pullandbear.net/2/photos/2022/V/0/1/p/4246/392/513/4246392513_1_1_3.jpg?t=1646392305779")
-        response = requests.get("http://" + host + product.picture)
-        img = Image.open(BytesIO(response.content))
-        img_t = data_transforms['val'](img).unsqueeze(0)
-        img_t = img_t.to(device)
-        outputs = model(img_t)
-        _, preds = torch.max(outputs, 1)
-        predicted = class_names[int(preds.cpu().numpy())]
+
+        predicted = Classifier.Predict(Classifier, host, product.picture)
         logger.info("predicted: {}".format(predicted))
 
         # build and return response
@@ -179,70 +161,9 @@ if __name__ == "__main__":
     demo_pb2_grpc.add_ClassifyingServiceServicer_to_server(service, server)
     health_pb2_grpc.add_HealthServicer_to_server(service, server)
 
-    # start classifier code
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    class_names = [
-        'Backpacks', 
-        'Belts', 
-        'Bra', 
-        'Briefs', 
-        'Caps', 
-        'Casual Shoes', 
-        'Clutches', 
-        'Deodorant', 
-        'Dresses', 
-        'Earrings', 
-        'Flats', 
-        'Flip Flops', 
-        'Formal Shoes', 
-        'Handbags', 
-        'Heels', 
-        'Jackets', 
-        'Jeans', 
-        'Kurtas', 
-        'Lipstick', 
-        'Nail Polish', 
-        'Perfume and Body Mist', 
-        'Sandals', 
-        'Sarees', 
-        'Shirts', 
-        'Shorts', 
-        'Socks', 
-        'Sports Shoes', 
-        'Sunglasses', 
-        'Sweaters', 
-        'Sweatshirts', 
-        'Ties', 
-        'Tops', 
-        'Track Pants', 
-        'Trousers', 
-        'Tshirts', 
-        'Wallets', 
-        'Watches'
-        ]
-    model_ft = models.resnet18(pretrained=True)
-    num_ftrs = model_ft.fc.in_features
-
-    #Changing the number of outputs in the last layer to the number of different item types
-    model_ft.fc = nn.Linear(num_ftrs, len(class_names))
-
-    model_ft = model_ft.to(device)
-    model = model_ft
-    model.load_state_dict(torch.load('model_fine_tuned.pt', device))
-    model.eval()
-
-    # Data augmentation and normalization for training
-    # Just normalization for validation
-    data_transforms = {
-        'val': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]),
-    }
-    torch.no_grad()
-    # end classifier code
+    # setup Classifier
+    classifier = Classifier()
+    classifier.Setup()
 
     # start server
     logger.info("listening on port: " + port)
--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v1.25 .
-docker push lvitroleruibk/boutique-classifier:v1.25
+docker build -t lvitroleruibk/boutique-classifier:v2.0 .
+docker push lvitroleruibk/boutique-classifier:v2.0
