b0be78f011ec93eb2f0a3f5bb49040f95aad2a1a
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-08-12 18:36:46 +0200

    feat: lets hope for traces

--- src/frontend/upload.sh
+++ src/frontend/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-frontend:v1.13 .
-docker push lvitroleruibk/boutique-frontend:v1.13
+docker build -t lvitroleruibk/boutique-frontend:v1.14 .
+docker push lvitroleruibk/boutique-frontend:v1.14
