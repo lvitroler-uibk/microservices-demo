2f17388693eaaf659d675d1782e0cbe4c7446f60
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-08-12 15:42:27 +0200

    feat: start with getting traces

--- export-logs/GetTraces.py
+++ export-logs/GetTraces.py
@@ -5,20 +5,11 @@ from datetime import date
 import requests
 from requests.structures import CaseInsensitiveDict
 import json
-import csv
 
 #list of all available microservices - necessary to query each
-allServices = ["ts-admin-basic-info-service", "ts-admin-order-service", "ts-admin-route-service",
-               "ts-admin-travel-service", "ts-admin-user-service", "ts-assurance-service", "ts-auth-service",
-               "ts-basic-service", "ts-cancel-service", "ts-config-service", "ts-consign-price-service",
-               "ts-consign-service", "ts-contacts-service", "ts-execute-service", "ts-food-map-service",
-               "ts-food-service", "ts-inside-payment-service", "ts-news-service", "ts-notification-service",
-               "ts-order-other-service", "ts-order-service", "ts-payment-service", "ts-preserve-other-service",
-               "ts-preserve-service", "ts-price-service", "ts-rebook-service", "ts-route-plan-service",
-               "ts-route-service", "ts-seat-service", "ts-security-service", "ts-station-service",
-               "ts-ticketinfo-service", "ts-ticket-office-service", "ts-train-service", "ts-travel2-service",
-               "ts-travel-plan-service", "ts-travel-service", "ts-user-service", "ts-verification-code-service",
-               "ts-voucher-service"]
+allServices = ["emailservice", "checkoutservice", "recommendationservice", "classifyingservice", "frontend",
+                "paymentservice", "productcatalogservice", "cartservice", "shippingservice", "adservice",
+                "frontend-external", "currencyservice"]
 URL = "http://jaeger-contest-contest.apps.play.gepaplexx.com/api/traces?service="  # ?service=ts-auth-service&prettyPrint=true" #traces für einen Service
 
 '''
--- export-logs/__init__.py
+++ export-logs/__init__.py
@@ -6,10 +6,10 @@ Authentication = "sha256~BhFivk7wCVvTgXPbw_6v9CwrWz9_YpDfvQr9EHSF-IY"
 
 if __name__ == "__main__":
     print("data is now getting collected")
-    called_version = "ts-admin-basic-info-service_springstarterweb_1.5.22.RELEASE"
+    called_version = "nofaults"
     #TODO alle Version Updates in einem Ordner zusammensammeln
 
-    mainLogs("LOGS_" + called_version + ".txt")
-    mainTraces(called_version)
+    #mainLogs("LOGS_" + called_version + ".txt")
+    #mainTraces(called_version)
     mainMonitoringData(called_version + ".json")
     print("Data Collection is finished!")
