37c7d95be188da546062e409fdb5af66509ac1c8
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-08-15 10:45:48 +0200

    chore: inject

--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v2.4 .
-docker push lvitroleruibk/boutique-classifier:v2.4
+docker build -t lvitroleruibk/boutique-classifier:v2.4-fault .
+docker push lvitroleruibk/boutique-classifier:v2.4-fault
