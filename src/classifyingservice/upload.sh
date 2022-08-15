7fe9968c2ebed6f3946701e2a2dea4ce95ab8dbc
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-08-15 11:49:50 +0200

    chore: fault 2nd try

--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v2.7 .
-docker push lvitroleruibk/boutique-classifier:v2.7
+docker build -t lvitroleruibk/boutique-classifier:v2.7-fault .
+docker push lvitroleruibk/boutique-classifier:v2.7-fault
