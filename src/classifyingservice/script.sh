2b063668f78b51f0dbffaa1dbc54e86aa5da49c1
lvitroler-uibk <lukas.vitroler@student.uibk.ac.at>
2022-08-15 11:43:12 +0200

    feat: restore previous version

--- src/classifyingservice/script.sh
+++ src/classifyingservice/script.sh
@@ -5,6 +5,6 @@ cp kaggle.json ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
 kaggle datasets download -d paramaggarwal/fashion-product-images-small
 unzip fashion-product-images-small.zip -d products 
-python data_setup.py
-python train.py
+python classifyingservice/data_setup.py
+python classifyingservice/train.py
 python classifyingservice/classifying_server.py
--- src/classifyingservice/upload.sh
+++ src/classifyingservice/upload.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t lvitroleruibk/boutique-classifier:v2.6 .
-docker push lvitroleruibk/boutique-classifier:v2.6
+docker build -t lvitroleruibk/boutique-classifier:v2.7 .
+docker push lvitroleruibk/boutique-classifier:v2.7
